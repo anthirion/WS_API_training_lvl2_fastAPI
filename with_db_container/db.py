53b7fb6f360269b2f69351221edc21adfacfda80
antoinethirion <antoine.thirion@wavestone.com>
2024-08-30 22:37:40 +0200

    Connect to db with .env file

--- with_db_container/db.py
+++ with_db_container/db.py
@@ -3,22 +3,10 @@ This module starts the database
 """
 
 from sqlalchemy import create_engine
-# Retrieve secrets from .env file
 from dotenv import dotenv_values
-
 from .models import Base
 
-
-# db_config = dotenv_values(".env")  # dictionnary
-
-
-db_config = {
-    "USER": "root",
-    "PSWD": "WavestoneApiTraining",
-    "HOST": "db",
-    "PORT": "3306",
-    "NAME": "api_training",
-}
+db_config = dotenv_values("app/.env")
 
 DATABASE_URL = (
     f"mysql://"
