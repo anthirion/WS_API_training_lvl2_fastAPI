cd3f3125e18c6ff67d12b49afd763b7de416e351
antoinethirion <antoine.thirion@wavestone.com>
2024-08-30 18:10:46 +0200

    Seperate bash commands in a sh file instead of writing them in yml file

---
+++ start_api_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# GREEN='\033[0;32m'
+# NC='\033[0m' # No Color
+
+# wait until db container is up and a connection can be created
+./wait-for-it/wait-for-it.sh db:3306 -- echo "DB SERVER IS UP"
+# launch the api on production mode
+fastapi run app/main.py
+# wait until api server is available on port 8000
+./wait-for-it/wait-for-it.sh apiserver:8000 -- echo "API SERVER IS UP"
