8e858b1c6e2da559f0d976864395bff0e5ccf611
antoinethirion <antoine.thirion@wavestone.com>
2024-09-06 14:14:31 +0200

    Modification du chemin .env

--- containerized_app/db.py
+++ containerized_app/db.py
@@ -7,6 +7,7 @@ from dotenv import dotenv_values
 from .models import Base
 
 db_config = dotenv_values("app/.env")
+# db_config = dotenv_values(".env")     # for test purpose
 
 DATABASE_URL = (
     f"mysql://"
