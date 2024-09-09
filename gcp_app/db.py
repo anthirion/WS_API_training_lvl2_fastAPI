13f056c600fdabfe10b95f71aa59cbd5f9ef959b
antoinethirion <antoine.thirion@wavestone.com>
2024-09-09 09:42:02 +0200

    Ajout d'un dossier pour le déploiement sur gcp

---
+++ gcp_app/db.py
@@ -0,0 +1,21 @@
+"""
+This module starts the database
+"""
+
+from sqlalchemy import create_engine
+from dotenv import dotenv_values
+from .models import Base
+
+db_config = dotenv_values("app/.env")
+# db_config = dotenv_values(".env")     # for test purpose
+
+DATABASE_URL = (
+    f"mysql://"
+    f"{db_config["USER"]}:{db_config["PSWD"]}"
+    f"@{db_config["HOST"]}:{db_config["PORT"]}"
+    f"/{db_config["NAME"]}"
+)
+
+engine = create_engine(DATABASE_URL)
+# create the table and the schema
+Base.metadata.create_all(engine)
--- start_api_server.sh
+++ start_api_server.sh
@@ -4,8 +4,9 @@
 # NC='\033[0m' # No Color
 
 # wait until db container is up and a connection can be created
-./wait-for-it/wait-for-it.sh db:3306 -- echo "DB SERVER IS UP"
+# the --strict argument executes the echo command only if the server is up
+./wait-for-it/wait-for-it.sh db:3306 --strict -- echo "DB SERVER IS UP"
 # launch the api on production mode
 fastapi run app/main.py
 # wait until api server is available on port 8000
-./wait-for-it/wait-for-it.sh apiserver:8000 -- echo "API SERVER IS UP"
+./wait-for-it/wait-for-it.sh apiserver:8000 --strict -- echo "API SERVER IS UP"
