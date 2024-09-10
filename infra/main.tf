89851d83ef7238b2e5615e0feaa11048ff5e4fa9
antoinethirion <antoine.thirion@wavestone.com>
2024-09-10 15:26:22 +0200

    Initialisation configuration via Ansible

--- infra/main.tf
+++ infra/main.tf
@@ -17,27 +17,27 @@ resource "google_compute_network" "vpc_network" {
   name = "private-network"
 }
 
-resource "google_compute_instance" "vm_api_server" {
-  name          =  "api-server"
-  machine_type  =  "e2-micro"
-  zone          =  var.zone
+# resource "google_compute_instance" "vm_api_server" {
+#   name          =  "api-server"
+#   machine_type  =  "e2-micro"
+#   zone          =  var.zone
 
-  boot_disk {
-    initialize_params {
-      # disk optimized for containers
-      image = "cos-cloud/cos-stable"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       # disk optimized for containers
+#       image = "cos-cloud/cos-stable"
+#     }
+#   }
 
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    # access_config parameter gives the VM an external IP address, making it accessible over
-    # the internet
-    # TO REMOVE
-    access_config {
-    }
-  }
-}
+#   network_interface {
+#     network = google_compute_network.vpc_network.name
+#     # access_config parameter gives the VM an external IP address, making it accessible over
+#     # the internet
+#     # TO REMOVE
+#     access_config {
+#     }
+#   }
+# }
 
 resource "google_compute_instance" "vm_mysql" {
   name          =   "db"
@@ -60,4 +60,22 @@ resource "google_compute_instance" "vm_mysql" {
     access_config {
     }
   }
+
+  # Tags pour identifier les règles de firewall à appliquer
+  tags = ["db-server"]
+}
+
+# Règle de firewall pour autoriser les connexions externes à MySQL (port 3306)
+resource "google_compute_firewall" "allow-sql" {
+  name    = "allow-port-3306"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]  # Autorise l'accès depuis toutes les adresses IP (à ajuster pour plus de sécurité)
+
+  target_tags = ["db-server"]  # Appliqué aux instances avec le tag "db-server"
 }
