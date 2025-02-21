a2dc610df609260643ab1eeeeee303fe844b3aa3
antoinethirion <antoine.thirion@wavestone.com>
2025-02-21 11:02:37 +0100

    chore: Suppression de fichiers et dossiers inutiles

--- docker_config/start_api_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# wait until db container is up and a connection can be created
-# the --strict argument executes the echo command only if the server is up
-./wait-for-it/wait-for-it.sh db:3306 --strict -- echo "DB SERVER IS UP"
-# launch the api on production mode
-fastapi run app/main.py
-# wait until api server is available on port 8000
-./wait-for-it/wait-for-it.sh apiserver:8000 --strict -- echo "API SERVER IS UP"
