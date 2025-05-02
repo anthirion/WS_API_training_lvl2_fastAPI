3b064eacba3f561ed5434d8a31eaf59fc19ffee3
antoinethirion <antoine.thirion@wavestone.com>
2025-05-02 10:44:50 +0200

    chore: moved gcp infra directory to a new branch

--- gcp_infra/gcp_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dest_dir="/home/anthirion/app"
-
-cd "$dest_dir"
-python3 -m venv api_venv
-source api_venv/bin/activate
-pip install -r requirements.txt
-fastapi run main.py --port 80 >> logs.txt &
