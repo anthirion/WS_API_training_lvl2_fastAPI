0e1b595260dc660999cb7b5d56dca488b5958763
antoinethirion <antoine.thirion@wavestone.com>
2025-05-05 14:35:05 +0200

    chore: change encoding type (from windows to linux)

--- gcp_install.sh
+++ gcp_install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-dest_dir="/home/anthirion/app"
-
-cd "$dest_dir"
-python3 -m venv api_venv
+python3 -m venv api_venv/
 source api_venv/bin/activate
 pip install -r requirements.txt
 fastapi run main.py --port 80 >> logs.txt &
